9b733f7e4edec3f6ebe8d59f06d1f2cbf95adb01
Yago  Aguiar <yagoaguiar@MacBook-Pro-de-Yago.local>
2023-06-19 14:50:23 -0300

    footer alteração #21

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './pages/Navbar'
-
+import Footer from './pages/Footer'
 
 function App() {
 
@@ -19,6 +19,7 @@ function App() {
       <Route path='/About' element={<About />} />
     </Routes>
     </div>
+    <Footer />
     </BrowserRouter>
    </div>
   )
