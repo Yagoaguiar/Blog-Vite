ba482b70e3a091a668f326a65ecb1dc8a86a9783
Yagoaguiar <yagoaguiar0203@gmail.com>
2023-06-19 16:59:36 -0300

    Rotas foram criadas #24

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Navbar from './pages/Navbar'
-import Footer from './pages/Footer'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
-
   return (
-   <div className='App'>
-    <BrowserRouter>
-    <Navbar /> 
-    <div className='container'> 
-    <Routes >
-      <Route path="/Home" element={<Home/>} />
-      <Route path='/About' element={<About />} />
-    </Routes>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
-    <Footer />
-    </BrowserRouter>
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
