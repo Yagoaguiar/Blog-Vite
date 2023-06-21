bdaf1a1e6d818b90f146bff98cf2631b03ed4cc2
Yagoaguiar <yagoaguiar0203@gmail.com>
2023-06-20 22:23:53 -0300

    validação do login do usuario colocando hook #31

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { AuthProvider } from "./content/AuthContext";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/Home" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
